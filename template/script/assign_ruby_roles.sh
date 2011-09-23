117112dc9877bb2da8d9827a075ae056f45b7864
David Vollbracht <david.vollbracht@gmail.com>
2011-09-23 11:58:58 -0400

    remove assign*roles shell scripts

--- template/script/assign_ruby_roles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-knife node run_list add $1 "role[appserver]" -a "role[host]"
-knife node run_list add $1 "role[ruby_appserver]" -a "role[appserver]"
-knife node run_list add $1 "role[webserver]" -a "role[ruby_appserver]"
-knife node run_list add $1 "role[queueserver]" -a "role[webserver]"
